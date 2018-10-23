7bc04f5bb387757d61051c9094aba6b37bc3ef77
VANSA PHA <vansa.jm@yahoo.com>
2018-10-23 20:18:23 +0700

    Initial Commit

---
+++ mmstudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mmstudio
+//
+//  Created by VANSA PHA on 10/23/18.
+//  Copyright © 2018 Vansa Pha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
